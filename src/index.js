9e01c5f100215205073041d04ef7122888bafdd8
Tom Rozendaal <tom.rozendaal@finalist.nl>
2020-01-08 16:35:47 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.renderBrowse = (containerId, history) => {
+  ReactDOM.render(<App history={history} />, document.getElementById(containerId))
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.unmountBrowse = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId))
+}
