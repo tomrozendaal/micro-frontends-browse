f550b860c63d9b28b9c27c7be8433388cf99a866
Tom Rozendaal <tom.rozendaal@finalist.nl>
2020-01-09 13:03:52 +0100

    t

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import './App.css'
 
 function App() {
+  const movies = [
+    {
+      id: 1,
+      title: 'One',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 2,
+      title: 'Two',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 3,
+      title: 'Three',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 4,
+      title: 'Four',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 5,
+      title: 'Five',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 6,
+      title: 'Six',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 7,
+      title: 'Seven',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    },
+    {
+      id: 8,
+      title: 'Eight',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container id="browse">
+      <Row>
+        {movies.map(movie => {
+          return (
+            <Col xs={3}>
+              <Card>
+                <Card.Img variant="top" src={movie.image} />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+    </Container>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 window.renderBrowse = (containerId, history) => {
   ReactDOM.render(<App history={history} />, document.getElementById(containerId))
 }
